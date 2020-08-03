d7509b4a842322160f39b29eeb3a463d449a90d6
Rafael Matoso <rafaelmatosog@gmail.com>
2020-08-02 21:30:29 -0300

    Adicionado botão de remover nome da lista

--- frontend/src/components/Body/index.tsx
+++ frontend/src/components/Body/index.tsx
@@ -7,7 +7,7 @@ interface ListProps {
 
 export const Body: React.FC = () => {
   const [name, setName] = useState('');
-  const [names, setNames] = useState<any>([]);
+  const [names, setNames] = useState<string[]>([]);
 
   const handleSubmit = (e: any): void => {
     e.preventDefault();
@@ -20,13 +20,27 @@ export const Body: React.FC = () => {
     setName(e.target.value);
   };
 
+  const handleRemoveName = (): void => {
+    const auxNames = [...names];
+    auxNames.shift();
+    setNames(() => [...auxNames]);
+  };
+
   return (
     <div className="App-body">
       <h2>Digite um Nome</h2>
       <form onSubmit={(e) => handleSubmit(e)}>
         <input type="text" name="name" onChange={(e) => handleInput(e)} />
-        <button type="submit" disabled={name === ''}>
+        <button className="adicionar" type="submit" disabled={name === ''}>
           Adicionar
+        </button>
+        <button
+          className="remover"
+          type="button"
+          onClick={() => handleRemoveName()}
+          disabled={names.length === 0}
+        >
+          Remover
         </button>
       </form>
       <List items={names} />
